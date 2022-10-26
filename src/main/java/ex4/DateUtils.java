fcb5399fa21e3609d891c78b9e2c1b4c83e10c9c
Carolanne Yvon <elolithia@gmail.com>
2022-10-26 17:54:28 +0200

    TP 01 Nettoyage - Ex 04

--- src/main/java/ex4/DateUtils.java
+++ src/main/java/ex4/DateUtils.java
@@ -5,15 +5,29 @@ import java.util.Date;
 
 public class DateUtils {
 
+	/** Pattern de formatage par défaut */
+	private static final String DEFAULT_FORMAT = "dd/MM/yyyy HH:mm:ss";
+
+	/**
+	 * Formatte la date en String avec un pattern imposé
+	 * 
+	 * @param date    date
+	 * @param pattern pattern
+	 * @return String
+	 */
 	public static String format(String pattern, Date date) {
-		
+
 		SimpleDateFormat formateur = new SimpleDateFormat(pattern);
 		return formateur.format(date);
 	}
-	
-	public static String formatDefaut(Date date) {
-		
-		SimpleDateFormat formateur = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-		return formateur.format(date);
+
+	/**
+	 * Formate la date en String avec le pattern par défaut
+	 * 
+	 * @param date date à formater
+	 * @return String
+	 */
+	public static String formatDefaut(Date date, String DEFAULT_FORMAT ) {
+		return formatDefaut(date, DEFAULT_FORMAT);
 	}
 }
